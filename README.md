51f01073242a9909bb6743f54c49514c8b931d2a
Carl-Erik Kopseng <carlerik@gmail.com>
2019-05-29 21:42:32 +0200

    Add working example

--- README.md
+++ README.md
@@ -1,68 +1,12 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Demo: shared header component
 
-## Available Scripts
+To avoid flickering when going from one screen to another in a single-page app, it is useful to try and keep as much of the DOM intact as possible, basically common hierarcies, especially if a sub-tree is moderately expensive to render. One such example could be a `<Header>` component in an app: shared by many, but is on the outside of each screen for performance.
 
-In the project directory, you can run:
+There are two issues that is interesting to ponder:
+1. How can we dynamically update the header along with the screen component - which is rendered elsewhere?
+2. How can we pass event handlers to the header when it is located further up the hierarcy?
 
-### `npm start`
+This demo tries to answer how to do both of these things. The first issue is basically solved using React Router: by having a router section in the header as well as in the screen component, we can update them both in sync with changes in the url. The problem of event handlers involves a bit more thinking, but is quite easy to solve in practice: we pass an object into both DOM hierarcies and mutate it to set new event handlers. The interface of such a shared object could be as simple as props we set a la `obj.myHandler = fooMyHandler`, but in our demo we have been a bit more fancy, ensuring the event handler name is recognized and that it actually expects an event as the parameter.
 
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.<br>
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.<br>
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+## Try it out
+`npm start`
--- src/App.js
+++ src/App.js
@@ -1,26 +1,135 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// This object exposes the header event handler API and
+// can be passed down to sub-components to change what will
+// happen in the header when that component has been mounted
+const headerController = (() => {
+  // our list of handlers
+  const handlers = {
+    handleButtonClick() {},
+    handleInputFocus() {}
+  };
+
+  // the api we will expose to our clients; a setter (used by the non-header-components) and the handlers (used by the header)
+  const api = {
+    setEventHandler(name, fn) {
+      if (!fn || fn.length < 1) {
+        throw new TypeError(
+          "An event handler needs to be supplied: function(event) {...}"
+        );
+      }
+
+      if (!(name in handlers)) {
+        throw new TypeError("Unknown event handler supplied");
+      }
+
+      handlers[name] = fn;
+    }
+  };
+
+  // expose our handlers, but use a layer in-between
+  // This is to make it possible to directly reference a handler in the components
+  // while at the same time being sure that a change in the handler config will be reflected
+  for (const handler of Object.keys(handlers)) {
+    api[handler] = e => handlers[handler](e);
+  }
+
+  // Ensure no mutation will take place
+  return Object.freeze(api);
+})();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header controls={headerController} />
+        <Route path="/bar" render={() => <Bar controls={headerController} />} />
+        <Route path="/foo" render={() => <Foo controls={headerController} />} />
+        <Route path="/" exact render={() => <Link to="/bar">Go to bar</Link>} />
+      </Router>
+      <p>This is the end, my friend</p>
     </div>
   );
+}
+
+function Header({ controls: c }) {
+  return (
+    <header className="App-header">
+      <Link to="/">
+        <img src={logo} className="App-logo" alt="logo" />
+      </Link>
+      <Route
+        path="/bar"
+        render={() => (
+          <p>
+            BarHeader
+            <button onClick={c.handleButtonClick}>Click me!</button>
+          </p>
+        )}
+      />
+      <Route
+        path="/foo"
+        render={() => (
+          <>
+            <p>FooHeader</p>
+            <input onFocus={e => c.handleInputFocus(e)} />
+          </>
+        )}
+      />
+      <Route path="/" exact render={() => `La-${"la-".repeat(10)}la`} />
+    </header>
+  );
+}
+
+class Bar extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    const { setEventHandler } = props.controls;
+
+    setEventHandler("handleButtonClick", event => {
+      alert("This is Barta!");
+      event.preventDefault();
+    });
+    setEventHandler("handleInputFocus", event => {
+      event.preventDefault();
+    });
+  }
+
+  render() {
+    return <Link to="/foo">Please focus!", said the Bar-tender</Link>;
+  }
+}
+
+let size = 10;
+let addToggle = true;
+function Foo(props) {
+  const { setEventHandler } = props.controls;
+
+  setEventHandler("handleInputFocus", event => {
+    const inputField = event.target;
+    inputField.value = "A Fooz pedal!";
+
+    // increase the font size on every event
+    const makeBig = e => {
+      size = addToggle ? size + 10 : size - 10;
+      e.target.style.fontSize = size + "px";
+
+      if (size > 50) addToggle = false;
+      if (size < 11) addToggle = true;
+    };
+
+    const removeMakeBig = () => {
+      inputField.removeEventListener("keyup", makeBig);
+    };
+
+    inputField.addEventListener("keyup", makeBig);
+    inputField.addEventListener("blur", removeMakeBig, { once: true }); // auto-cleanup
+  });
+
+  return <Link to="/">"I AM focused!", said the Foo-baller</Link>;
 }
 
 export default App;
